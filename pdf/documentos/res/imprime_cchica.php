c2eeae024b2b684a1e0bd5fb3f37437957f9f735
adminproconty <christian.teran@proconty.com>
2018-08-20 17:23:02 -0500

    login

--- pdf/documentos/res/imprime_cchica.php
+++ pdf/documentos/res/imprime_cchica.php
@@ -1,10 +1,5 @@
 <?php
-	session_start();
-    date_default_timezone_set('America/Bogota');
-	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
-        header("location: ../login.php");
-		exit;
-    }
+
     /* Connect To Database*/
 	include("../config/db.php");
 	include("../config/conexion.php");
